c70ce6babcef2c0d29a04eac56cc082e3218a41a
Mo <31892724+mo-rob-1@users.noreply.github.com>
2022-01-07 18:20:40 +0000

    add link to title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 function Header({ text, bgColor, textColor }) {
   const headerStyles = {
@@ -9,7 +10,11 @@ function Header({ text, bgColor, textColor }) {
   return (
     <header style={headerStyles}>
       <div className="container">
-        <h2>{text}</h2>
+        <h2>
+          <Link className="HeaderLink" exact to="/">
+            {text}
+          </Link>
+        </h2>
       </div>
     </header>
   );
